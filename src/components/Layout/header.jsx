1d235cf36dd3687491586664a3d2c8de3868cb65
khuzaimazammarud <khuzaimazammarud@gmail.com>
2024-09-25 23:19:49 +0500

    build fix

--- src/components/Layout/header.jsx
+++ src/components/Layout/header.jsx
@@ -4,9 +4,6 @@ function Header() {
   return (
     <header className="bg-blue-500 p-4 text-white text-center">
       <h1 className="text-2xl font-bold">Todo App</h1>
-      <nav>
-        <a href="/" className="hover:underline">Home</a>
-      </nav>
     </header>
   );
 }
