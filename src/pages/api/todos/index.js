a6f7b545d4137294819a7c96347b4149a62a3470
khuzaimazammarud <khuzaimazammarud@gmail.com>
2024-09-26 00:06:21 +0500

    connection string optimization

--- src/pages/api/todos/index.js
+++ src/pages/api/todos/index.js
@@ -1,10 +1,8 @@
-import connectMongo from '../../../lib/mongodb';
-import Todo from '../../../models/Todo';
+import connectMongo from "../../../lib/mongodb";
+import Todo from "../../../models/Todo";
 
 // POST Method Handler
 async function POST(req, res) {
-  await connectMongo();
-
   try {
     const todo = await Todo.create(req.body);
     res.status(201).json({ success: true, data: todo });
@@ -15,8 +13,6 @@ async function POST(req, res) {
 
 // GET Method Handler
 async function GET(req, res) {
-  await connectMongo();
-
   try {
     const todos = await Todo.find({});
     res.status(200).json({ success: true, data: todos });
@@ -27,14 +23,15 @@ async function GET(req, res) {
 
 // Main API Route Handler
 export default async function handler(req, res) {
+  await connectMongo();
   const { method } = req;
 
-  if (method === 'POST') {
+  if (method === "POST") {
     return POST(req, res);
-  } else if (method === 'GET') {
+  } else if (method === "GET") {
     return GET(req, res);
   } else {
-    res.setHeader('Allow', ['GET', 'POST']);
+    res.setHeader("Allow", ["GET", "POST"]);
     res.status(405).end(`Method ${method} Not Allowed`);
   }
 }
